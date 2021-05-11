fa9908f1e522b5d9c43470d07a580acb921d2365
Brady <bradyrcotton@gmail.com>
2021-05-10 21:22:23 -0500

    added post, delete, and put request

--- flashcards/flash_cards/urls.py
+++ flashcards/flash_cards/urls.py
@@ -5,5 +5,6 @@ from. import views
 app_name = 'flash_cards'
 urlpatterns = [
     path('flash_cards/', views.CardList.as_view()),
-    path('collection/', views.CollectionList.as_view())
+    path('collection/', views.CollectionList.as_view()),
+    path('flash_cards/<int:pk>/', views.CardList.as_view())
 ]
--- flashcards/flash_cards/views.py
+++ flashcards/flash_cards/views.py
@@ -21,7 +21,17 @@ class CardList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    def delete(self, request, pk):
+        card = self.get_object(pk)
+        card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self, request, pk):
+        card = self.get_object(pk)
+        serializer = CardSerializer(card, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CollectionList(APIView):
 
