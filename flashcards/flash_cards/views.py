38daea8e054c8477ec3c0a92da698110970e617e
Brady <bradyrcotton@gmail.com>
2021-05-10 21:26:13 -0500

    added a get object to Cardslist

--- flashcards/flash_cards/views.py
+++ flashcards/flash_cards/views.py
@@ -14,17 +14,18 @@ class CardList(APIView):
         serializer = CardSerializer(card, many=True)
         return Response(serializer.data)
 
-
     def post(self, request):
         serializer = CardSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk):
         card = self.get_object(pk)
         card.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
     def put(self, request, pk):
         card = self.get_object(pk)
         serializer = CardSerializer(card, data=request.data)
@@ -33,6 +34,13 @@ class CardList(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def get_object(self, pk):
+        try:
+            return Card.objects.get(pk=pk)
+        except Card.DoesNotExist:
+            raise Http404
+
+        
 class CollectionList(APIView):
 
     def get(self, request):
